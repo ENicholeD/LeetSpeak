3d3abd9947045bad1127c0407d8f79bbc64a6132
Erin Deuel <end.gaia@gmail.com>
2019-09-30 21:38:14 -0700

    create working program, next need to create working test

---
+++ LeetSpeak.Tests/ModelTests/LeetSpeakTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetSpeak;
+
+
+namespace LeetSpeak.Tests
+{
+  [TestClass]
+  public class LeetSpeakTest
+  {
+    public void Traslator()
+    {
+
+    }
+  }
+}
